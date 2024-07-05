cad0fc5e5ee0c6f7ce346236c44bf83105546ef6
Richard Azucenas <79331981+iRimpo@users.noreply.github.com>
2024-07-05 11:00:45 -0700

    Updated Demo

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ initial_height_percent = 70
 initial_width_percent = 70
 
 # Create a Pyvis Network object
-net = Network(height='500px', width='70%', neighborhood_highlight=True, select_menu=True, bgcolor="white", font_color="black", filter_menu=True)
+net = Network(height='500px', width='70%', neighborhood_highlight=True, select_menu=True, bgcolor="white", font_color="black", filter_menu=True, layout=False)
 
 # Call diagrammers to generate network diagrams
-# webctrl(demo, output_file_path, net=net)
+# webctrl(demo, demo_path, net=net)
 webctrl(webctrl_data, output_file_path, net=net)
 metasys(metasys_data, output_file_path, net=net)
 lutron(lutron_data, output_file_path, net=net)
